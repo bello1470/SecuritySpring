bb5e009635e231a617a0cbbbc894ec457acabfca
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-02-24 14:14:17 +0000

    commit

---
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/SpringSecurityRoleBaseApplication.java
@@ -0,0 +1,13 @@
+package org.bellotech.SpringSecurityRoleBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRoleBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRoleBaseApplication.class, args);
+	}
+
+}
