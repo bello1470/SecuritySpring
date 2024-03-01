7a59cd4637b52d896d8beaebc9f32f843a17b967
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-01 05:12:50 +0000

    controller commit

---
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/controller/UserController.java
@@ -0,0 +1,22 @@
+package org.bellotech.SpringSecurityRoleBase.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+    @GetMapping("/admin")
+    public String admin(){
+
+        return "Wellcome to ADMIN PAGE";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+
+        return "Wellcome to MANAGER PAGE";
+    }
+}
