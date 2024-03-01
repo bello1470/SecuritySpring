98a7199849e2e48139ec5733e88245501b5645a3
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-01 16:49:26 +0000

    services commit

---
+++ src/test/java/org/bellotech/SpringSecurityRoleBase/SpringSecurityRoleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bellotech.SpringSecurityRoleBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRoleBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
