d5122ae8bb7721486923af8aa67e92fd7ce018c7
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-06 20:58:00 +0000

    service details contract  commit

---
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/services/SecurityUser.java
@@ -0,0 +1,50 @@
+package org.bellotech.SpringSecurityRoleBase.services;
+
+import lombok.AllArgsConstructor;
+import org.bellotech.SpringSecurityRoleBase.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class SecurityUser implements UserDetails {
+
+
+    private  final User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
