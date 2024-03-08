04d082504a88fc1a18b6dc58bd9344fa7d2f22f6
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-08 16:15:12 +0000

    property  commit

---
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/entities/Authorities.java
@@ -0,0 +1,23 @@
+package org.bellotech.SpringSecurityRoleBase.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Table(name = "authorities")
+@Entity
+@Data
+public class Authorities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String names;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+
+}
