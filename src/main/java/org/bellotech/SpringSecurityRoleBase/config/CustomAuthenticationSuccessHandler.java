e5beb28dd927c26447a793c3e4ddc8fdc0f6b0a1
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-09 19:09:53 +0000

    comment  commit

--- src/main/java/org/bellotech/SpringSecurityRoleBase/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/config/CustomAuthenticationSuccessHandler.java
@@ -1,32 +1,32 @@
-package org.bellotech.SpringSecurityRoleBase.config;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
-
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
-        var roles = authentication.getAuthorities();
-
-        var role = roles.stream().map(r -> r.getAuthority()).findFirst();
-
-        if (role.orElse("").equals("ROLE_ADMIN")){
-
-            response.sendRedirect("/admin");
-        } else if (role.orElse("").equals("ROLE_MANAGER")) {
-
-            response.sendRedirect("/manager");
-
-        }else {
-
-            response.sendRedirect("/error");
-        }
-
-    }
-}
+//package org.bellotech.SpringSecurityRoleBase.config;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//
+//import java.io.IOException;
+//
+//public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//
+//        var roles = authentication.getAuthorities();
+//
+//        var role = roles.stream().map(r -> r.getAuthority()).findFirst();
+//
+//        if (role.orElse("").equals("ROLE_ADMIN")){
+//
+//            response.sendRedirect("/admin");
+//        } else if (role.orElse("").equals("ROLE_MANAGER")) {
+//
+//            response.sendRedirect("/manager");
+//
+//        }else {
+//
+//            response.sendRedirect("/error");
+//        }
+//
+//    }
+//}
