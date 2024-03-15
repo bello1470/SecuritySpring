ae385bebd877fdb487935487fd187ae8c10bdade
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-15 21:04:56 +0000

    sec commit

--- src/main/java/org/bellotech/SpringSecurityRoleBase/security/SecurityAuthority.java
+++ src/main/java/org/bellotech/SpringSecurityRoleBase/security/SecurityAuthority.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 @AllArgsConstructor
 public class SecurityAuthority implements GrantedAuthority {
-
+// auth set
     private final Authorities authorities;
     @Override
     public String getAuthority() {
